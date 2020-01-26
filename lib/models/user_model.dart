69062c36f2f8b0abb70886e4a937dd5457bb4f85
Bruno Degan <bmdegan@gmail.com>
2020-01-26 17:02:04 -0400

    implemented loadCurrentUser

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -9,6 +9,21 @@ class UserModel extends Model {
   FirebaseUser firebaseUser;
   Map<String, dynamic> userData;
 
+  @override
+  void addListener(VoidCallback listener) {
+    super.addListener(listener);
+    loadCurrentUser();
+  }
+
+  Future<Null> saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+
+    await Firestore.instance
+        .collection("users")
+        .document(firebaseUser.uid)
+        .setData(userData);
+  }
+
   Future<void> signUp(
       {@required Map<String, dynamic> userData,
       @required String pass,
@@ -17,30 +32,62 @@ class UserModel extends Model {
     isLoading = true;
     notifyListeners();
 
-    await firebaseAuth.createUserWithEmailAndPassword(
-        email: userData["email"], password: pass);
+    await firebaseAuth
+        .createUserWithEmailAndPassword(
+            email: userData["email"], password: pass)
+        .then((user) {
+      firebaseUser = user.user;
+      saveUserData(userData);
 
-    await saveUserData(userData).then((user) async {
-      firebaseUser = user;
-
-      if (firebaseUser != null) {
-        onSuccess();
-      } else {
-        onFailed();
-      }
+      onSuccess();
+      isLoading = false;
+    }).catchError((onError) {
+      onFailed();
       isLoading = false;
       notifyListeners();
     });
   }
 
-  void signIn() async {
+  Future<Null> loadCurrentUser() async {
+    if (firebaseUser == null)
+      firebaseUser = await firebaseAuth.currentUser();
+
+    if (firebaseUser != null) {
+      if (userData["name"] == null) {
+        DocumentSnapshot docUser = await Firestore.instance
+            .collection("users")
+            .document(firebaseUser.uid)
+            .get();
+        userData = docUser.data;
+      }
+    }
+
+    notifyListeners();
+  }
+
+  void signIn(
+      {@required var email,
+      @required var password,
+      @required VoidCallback onSuccess,
+      @required VoidCallback onFailed}) async {
     isLoading = true;
     notifyListeners();
 
-    await Future.delayed(Duration(seconds: 3));
+    firebaseAuth
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((authResult) async {
+      firebaseUser = authResult.user;
 
-    isLoading = false;
-    notifyListeners();
+      await loadCurrentUser();
+
+      onSuccess();
+      notifyListeners();
+      isLoading = false;
+    }).catchError((error) {
+      onFailed();
+      notifyListeners();
+      isLoading = false;
+    });
   }
 
   void recoverPassword() {}
@@ -49,20 +96,10 @@ class UserModel extends Model {
     return firebaseUser != null;
   }
 
-  Future<Null> saveUserData(Map<String, dynamic> userData) async {
-    this.userData = userData;
-
-    Firestore.instance
-        .collection("users")
-        .document(firebaseUser.uid)
-        .setData(userData);
-  }
-
   void signOut() async {
     await firebaseAuth.signOut();
     userData = Map();
     firebaseUser = null;
     notifyListeners();
   }
-
 }
