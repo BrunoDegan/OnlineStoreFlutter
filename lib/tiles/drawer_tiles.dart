efc17d97c895aae2690086d67ef5e9a4153e415d
Bruno Degan <bmdegan@gmail.com>
2020-01-27 00:54:34 -0400

    added on tap event

--- lib/tiles/drawer_tiles.dart
+++ lib/tiles/drawer_tiles.dart
@@ -1,8 +1,6 @@
 import "package:flutter/material.dart";
 
-
 class DrawerTiles extends StatelessWidget {
-
   final IconData icon;
   final String text;
   final PageController _pageController;
@@ -15,34 +13,36 @@ class DrawerTiles extends StatelessWidget {
     return Material(
       color: Colors.transparent,
       child: InkWell(
-        onTap:() {
-          Navigator.of(context).pop();
-          _pageController.jumpToPage(page);
-        },
-        child: Container(
-          height: 60.0,
-          child: Row(
-            children: <Widget>[
-              Icon(
-                icon,
-                size: 32.0,
-                color: _pageController.page.round() == this.page ?
-                  Theme.of(context).primaryColor : Colors.grey[700],
-              ),
-
-              SizedBox(width: 32.0),
-              Text(
-                text,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.black
+          onTap: () {
+            Navigator.of(context).pop();
+            _pageController.jumpToPage(page);
+          },
+          child: Container(
+            height: 60.0,
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  icon,
+                  size: 32.0,
+                  color: getColor(context),
                 ),
-              ),
-            ],
-          ),
-        )
-      ),
+                SizedBox(width: 32.0),
+                Text(
+                  text,
+                  style: TextStyle(fontSize: 16.0, color: Colors.black),
+                ),
+              ],
+            ),
+          )),
     );
   }
 
+  Color getColor(context) {
+    var color = Colors.grey[700];
+
+    if (_pageController.page.round() == this.page)
+      color = Theme.of(context).primaryColor;
+
+    return color;
+  }
 }
