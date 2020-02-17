2f8094c4b9598f5d90f1ac002ec1da5d623c3d86
Bruno <bruno.degan@itbam.org.br>
2020-02-17 10:24:50 -0400

    Implemented Order tiles status update

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -17,39 +17,90 @@ class OrderTile extends StatelessWidget {
             child: StreamBuilder<DocumentSnapshot>(
                 stream: Firestore.instance
                     .collection("orders")
-                    .document("orderId")
+                    .document(orderId)
                     .snapshots(),
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) {
                     return CircularProgressIndicator();
                   } else {
+                    int status = snapshot.data["status"];
+
                     return Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Text("Código do pedido: ${snapshot.data.documentID}",
                             style: TextStyle(fontWeight: FontWeight.bold)),
-                        SizedBox(
-                          height: 4.0,
-                        ),
-                        Text(buildProductsTab(snapshot.data))
+                        SizedBox(height: 4.0,),
+                        Text(buildProductsTab(snapshot.data)),
+                        SizedBox(height: 4.0,),
+                        Text("Status do pedido",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(height: 4.0,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            buildOrderStatus("1", "Preparação",status, 1),
+                            Container(height: 1.0, width: 40.0, color: Colors.grey,),
+                            buildOrderStatus("2", "Transporte", status, 2),
+                            Container(height: 1.0, width: 40.0, color: Colors.grey,),
+                            buildOrderStatus("2", "Entrega", status, 3)
+                          ],
+                        )
                       ],
                     );
                   }
                 })));
   }
 
-
   String buildProductsTab(DocumentSnapshot snapshot) {
     String formattedDescription = "Descrição:\n";
+
     for (LinkedHashMap map in snapshot.data["products"]) {
       formattedDescription +=
-      "${map["quantity"]} x ${map["product"]["title"]} {R\$ ${map["product"]["price"].toString()}\n";
-      formattedDescription += "Total: R\$ ${snapshot.data["totalPrice"].toString()}";
+          "${map["quantity"]} x ${map["product"]["title"]} R\$ ${map["product"]["price"].toString()}\n";
+      formattedDescription +=
+          "Total: R\$ ${snapshot.data["totalPrice"].toString()}";
     }
 
     return formattedDescription;
   }
 
+  Widget buildOrderStatus(
+      String title, String subtitle, int status, thisStatus) {
+
+    Color backColor;
+    Widget child;
+
+    if (status < thisStatus) {
+      backColor = Colors.grey[500];
+      child = Text(title, style: TextStyle(color: Colors.white));
+    }
+
+    if (status == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Text(title, style: TextStyle(color: Colors.white)),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          )
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = Icon(Icons.check, color: Colors.white,);
+    }
+
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 20.0,
+          backgroundColor: backColor,
+          child: child,
+        ),
+        Text(subtitle)
+      ],
+    );
+  }
 }
-
-
