30d7c6b3a85006dc2e1d4728a7d5bf504406587a
Bruno <bruno.degan@itbam.org.br>
2020-02-17 19:13:52 -0400

    Added dialog for exiting app

--- lib/tabs/order_tab.dart
+++ lib/tabs/order_tab.dart
@@ -5,11 +5,36 @@ import 'package:onlinestore/screens/login_screen.dart';
 import 'package:onlinestore/tiles/order_tile.dart';
 
 class OrderTabs extends StatelessWidget {
+
+  BuildContext appContext;
+
+  Future<bool> onWillPop() async {
+    return (await showDialog(
+      context: appContext,
+      builder: (context) => new AlertDialog(
+        title: new Text('Você tem certeza?'),
+        content: new Text('Deseja sair do app'),
+        actions: <Widget>[
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: new Text('No'),
+          ),
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: new Text('Yes'),
+          ),
+        ],
+      ),
+    )) ?? false;
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    appContext = context;
     if (UserModel.of(context).isLoggedIn()) {
       String uid = UserModel.of(context).firebaseUser.uid;
-      return new FutureBuilder<QuerySnapshot>(
+      return new WillPopScope(child: FutureBuilder<QuerySnapshot>(
           future: Firestore.instance
               .collection("users")
               .document(uid)
@@ -18,7 +43,7 @@ class OrderTabs extends StatelessWidget {
           builder: (context, snapshot) {
             if (!snapshot.hasData) {
               return Center(
-               child: CircularProgressIndicator()
+                  child: CircularProgressIndicator()
               );
             } else {
               return ListView(
@@ -26,7 +51,7 @@ class OrderTabs extends StatelessWidget {
                       .map((doc) => OrderTile(doc.documentID))
                       .toList().reversed.toList());
             }
-          });
+          }), onWillPop: onWillPop);
     } else {
       return Container(
         padding: EdgeInsets.all(16.0),
